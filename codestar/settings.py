9bf11481a1b777ce09bf8c73ccff964015ffcc83
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2024-02-06 17:59:32 +0000

    Update settings.py with new key/links

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-davidfb94-djangoblog-to82igrq1j1.ws-eu108.gitpod.io','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-davidfb94-djangoblog-8gdrjx6dzpk.ws-eu108.gitpod.io','.herokuapp.com']
 
 
 # Application definition
@@ -90,6 +90,7 @@ DATABASES = {
 }
 
 CSRF_TRUSTED_ORIGINS = [
+    "https://*.gitpod.io",
     "https://*.codeanyapp.com",
     "https://*.herokuapp.com"
 ]
